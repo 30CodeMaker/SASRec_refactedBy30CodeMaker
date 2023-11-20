c5b9fb60c32ae8c329a12b0aeb04023db5c5520e
30CodeMaker <uglyyycurry@gmail.com>
2023-11-20 15:58:30 +0800

    upload SASRec refactored by 30CodeMaker

---
+++ main.py
@@ -0,0 +1,126 @@
+import time
+import torch
+from model import SASRec
+from utils import *
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+
+
+# 用于绘制图像查看迭代过程中的NDCG和HR
+def plot_and_save(x, y, title, filename):
+    plt.plot(x, y)
+    plt.title(title)
+    plt.savefig(filename)
+    plt.clf()
+
+
+# 自定义数据集 用于dataloader分批次读取数据
+class TrainDataset(Dataset):
+    def __init__(self, train_data, sequence_length, usernum, itemnum):
+        self.train_data = train_data
+        self.sequence_length = sequence_length
+        self.usernum = usernum
+        self.itemnum = itemnum
+
+    def __len__(self):
+        return len(self.train_data)
+
+    def random_neg(self, l, r, ts):
+        t = np.random.randint(l, r)
+        while t in ts:
+            t = np.random.randint(l, r)
+        return t
+
+    def __getitem__(self, index):
+        # 注意这里的getitem是用于dataloader读取数据的，所以这里的index是dataloader传入的，但是这里的index是无用的，因为我们是随机取的数据
+        userid = np.random.randint(1, self.usernum + 1)
+        while len(self.train_data[userid]) <= 1: userid = np.random.randint(1, self.usernum + 1)
+
+        seq = np.zeros([self.sequence_length], dtype=np.int32)
+        pos = np.zeros([self.sequence_length], dtype=np.int32)
+        neg = np.zeros([self.sequence_length], dtype=np.int32)
+
+        # 根据论文中的描述，我们需要将序列中的物品分为正样本和负样本，正样本就是序列中的物品，负样本就是随机抽取的物品
+        # 而我们的序列 你可以发现 seq和pos是不一样的 seq是序列中的物品，而pos是seq序列向后移动一位的结果，这样就可以得到正样本
+        # 也就是说 pos是真正训练集序列的后50个物品，而seq要整体向左移动一位 因为要以seq预测pos
+        # 根据原论文  假设我们取的序列长度是50，而真实序列长度不够50  则记得再前面补0 即填充项，然后在后续的处理中过滤掉填充项
+        offset_item = self.train_data[userid][-1]
+        idx = self.sequence_length - 1
+        ts = set(self.train_data[userid])
+        for i in reversed(self.train_data[userid][:-1]):
+            seq[idx] = i
+            pos[idx] = offset_item
+            if offset_item != 0: neg[idx] = self.random_neg(1, self.itemnum + 1, ts)
+            offset_item = i
+            idx -= 1
+            if idx == -1: break
+        return (userid, seq, pos, neg)
+
+
+if __name__ == '__main__':
+    path = "./data/ml-1m.txt"
+    sequence_length = 50
+    batch_size = 128
+    epochs = 200
+    hidden_units = 50
+    dropout_rate = 0.3
+    num_of_blocks = 2
+    num_of_heads = 1
+    learning_rate = 0.001
+
+    [train_data, valid_data, test_data, user_num, item_num] = load_data(path)
+    print("finish loading data...")
+    train_dataset = TrainDataset(train_data, sequence_length, user_num, item_num)
+    print("finish creating dataset...")
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+    print("start training...")
+
+    model = SASRec(user_num, item_num, hidden_units, dropout_rate, sequence_length, num_of_blocks, num_of_heads)
+    # 初始化权重
+    for name, param in model.named_parameters():
+        try:
+            torch.nn.init.xavier_normal_(param.data)
+        except:
+            pass  # just ignore those failed init layers
+    bce_criterion = torch.nn.BCEWithLogitsLoss()
+    adam_optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.98))
+
+    model.train()  # 训练模式
+    t0 = time.time()
+
+    epoch_list = []
+    NDCG_list = []
+    HR_list = []
+
+    num = 0
+    f = open(f"./saved_results/result{num}.txt", "w")
+    for epoch in range(1, epochs + 1):
+        print(epoch)
+        for userid, seq, pos, neg in train_dataloader:
+            # 分别根据数据对取出来的正样本和负样本进行预测
+            pos_predictions, neg_predictions = model(seq, pos, neg)
+            pos_labels = torch.ones_like(pos_predictions)
+            neg_labels = torch.zeros_like(neg_predictions)
+
+            adam_optimizer.zero_grad()
+            real_labels_index = np.where(pos != 0) # 过滤掉填充项
+            loss = bce_criterion(pos_predictions[real_labels_index], pos_labels[real_labels_index])
+            loss += bce_criterion(neg_predictions[real_labels_index], neg_labels[real_labels_index])
+            loss.backward()
+            adam_optimizer.step()
+
+        if epoch % 10 == 0:
+            epoch_list.append(epoch)
+            NDCG, HR = evaluate(model, [train_data, valid_data, test_data, user_num, item_num], sequence_length)
+            NDCG_list.append(NDCG)
+            HR_list.append(HR)
+            print("epoch: ", epoch, " NDCG: ", NDCG, " HR: ", HR)
+            f.write(f"epoch: {epoch}, NDCG: {NDCG},  HR: {HR},  loss: {loss.item()}\n")
+            f.flush()
+    f.close()
+
+    plot_and_save(epoch_list, NDCG_list, "NDCG", "./saved_results/NDCG.png")
+    plot_and_save(epoch_list, HR_list, "HR", "./saved_results/HR.png")
+    print("Finished....")
+    plt.close()
+
---
+++ model.py
@@ -0,0 +1,122 @@
+import time
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+# SASRec network————FFN
+class FFN(nn.Module):
+    def __init__(self, hidden_units, dropout_rate):
+        super(FFN, self).__init__()
+        self.conv1 = nn.Conv1d(hidden_units, hidden_units, kernel_size=1)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.conv2 = nn.Conv1d(hidden_units, hidden_units, kernel_size=1)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_of_Attention):
+        outputs = self.conv2(self.dropout(self.relu(self.conv1(input_of_Attention.transpose(-1, -2)))))
+        outputs = outputs.transpose(-1, -2)
+        return outputs
+
+
+class SASRec(nn.Module):
+    def __init__(self, user_num, item_num, hidden_units, dropout_rate, sequence_length, num_of_blocks, num_of_heads):
+        super(SASRec, self).__init__()
+        self.user_num = user_num
+        self.item_num = item_num
+        self.item2emb = nn.Embedding(self.item_num + 1, hidden_units, padding_idx=0)
+        self.position2emb = nn.Embedding(sequence_length, hidden_units)
+        self.sequence_length = sequence_length
+
+        # 根据原论文构建的block 由于block的数量是可变的，所以这里使用了ModuleList
+        # 以下分别是多头注意力机制和FFN的layerNorm、dropout和layer
+        self.layerNorm_of_multi_head_attention_layers = nn.ModuleList()
+        self.multi_head_attention_layers = nn.ModuleList()
+        self.dropout_of_multi_head_attention_layers = nn.ModuleList()
+
+        self.layerNorm_of_FFN_layers = nn.ModuleList()
+        self.FFN_layers = nn.ModuleList()
+        self.dropout_of_FFN_layers = nn.ModuleList()
+
+        # 根据块的数量构建block
+        for _ in range(num_of_blocks):
+            self.layerNorm_of_multi_head_attention_layers.append(nn.LayerNorm(hidden_units, eps=1e-8))
+            self.multi_head_attention_layers.append(nn.MultiheadAttention(hidden_units, num_of_heads, dropout_rate))
+            self.dropout_of_multi_head_attention_layers.append(nn.Dropout(dropout_rate))
+
+            self.layerNorm_of_FFN_layers.append(nn.LayerNorm(hidden_units, eps=1e-8))
+            self.FFN_layers.append(FFN(hidden_units, dropout_rate))
+            self.dropout_of_FFN_layers.append(nn.Dropout(dropout_rate))
+
+    def sasRecBlock(self, i, inputs, attention_mask):
+        # 根据原论文构建sasRecBlock
+        inputs = torch.transpose(inputs, 0, 1)
+        # 先进行layerNorm然后再进行multi_head_attention多头注意力机制最后再进行dropout
+        inputs_norm = self.layerNorm_of_multi_head_attention_layers[i](inputs)
+        attention_output, _ = self.multi_head_attention_layers[i](inputs_norm, inputs, inputs,
+                                                                  attn_mask=attention_mask)
+        attention_output = self.dropout_of_multi_head_attention_layers[i](attention_output)
+        # residual connection 残差连接
+        attention_output += inputs
+
+        # 先进行layerNorm然后再进行FFN最后再进行dropout
+        inputs_norm = self.layerNorm_of_FFN_layers[i](attention_output)
+        outputs = self.FFN_layers[i](inputs_norm)
+        outputs = self.dropout_of_FFN_layers[i](outputs)
+        # residual connection 残差连接
+        outputs += attention_output
+        outputs = torch.transpose(outputs, 0, 1)
+        return outputs
+
+    #这个方法用于得到结果n个原论文构建的block的输出
+    def getF(self,seqs):
+        # 取出不同批次中序列中出现的物品的embedding
+        seqs_emb = self.item2emb(seqs)
+        # 取出不同批次中序列中物品位置的embedding
+        positions = np.tile(np.array(range(seqs.shape[1])), [seqs.shape[0], 1])
+        # 将物品位置的embedding加到物品的embedding中
+        seqs_emb += self.position2emb(torch.LongTensor(positions))
+
+        # 设置去除填充项的mask 用于过滤不存在的序列片段
+        time_line_mask = torch.BoolTensor(seqs == 0)
+
+        # 下三角过滤器 但是为什么要取反呢？？？？？ 不是要保留下三角吗，为什么要把下三角删了  查询发现原来是true表示不使用
+        # 至于为什么要只保留下三角，是因为要保证序列的顺序，不能让后面的物品出现在前面 即不能以未来预测现在，只能以过去预测未来，更详细的解释可以参考原论文
+        attention_mask = ~torch.tril(torch.ones(self.sequence_length, self.sequence_length, dtype=torch.bool))
+
+        for i in range(len(self.multi_head_attention_layers)):
+            #过滤不存在的序列片段
+            seqs_emb *= ~time_line_mask.unsqueeze(-1)
+            #经过block的处理后得到的结果
+            seqs_emb = self.sasRecBlock(i, seqs_emb, attention_mask)
+        return seqs_emb
+
+    def forward(self, seqs, pos_samples, neg_samples):
+        seqs_emb = self.getF(seqs)#得到对应论文中的F块
+
+        pos_samples_emb = self.item2emb(pos_samples)
+        neg_samples_emb = self.item2emb(neg_samples)
+
+        #根据原论文的预测公式对正负样本进行预测
+        pos_predictions = torch.sum(seqs_emb * pos_samples_emb, -1)
+        neg_predictions = torch.sum(seqs_emb * neg_samples_emb, -1)
+
+        return pos_predictions, neg_predictions
+
+    def predict(self, log_seqs, item_indices): # for inference
+
+        log_feats = self.getF(torch.LongTensor(log_seqs)) #取出该序列对应的embedding
+        #预测下一个物品只需要取出最后一个序列步的embedding即可，与原论文保持一致
+        # 1*50
+        final_feat = log_feats[:, -1, :]
+
+        #取出物品的embedding,注意这里应是传入了一个列表，列表中的元素是物品的id，一共有101个物品，其中只有一个物品是正样本，其余都是负样本
+        item_embs = self.item2emb(torch.LongTensor(item_indices))
+        #这里交换维度，为了后续的矩阵乘法
+        item_embs = item_embs.transpose(0, 1)
+
+        # 根据原论文的预测公式进行预测
+        predictions = final_feat.matmul(item_embs)
+        return predictions
+
